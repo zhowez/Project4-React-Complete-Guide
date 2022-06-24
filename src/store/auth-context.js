194deda3bfbe10020c0021a96389e7aeb6967a3e
Zach Howes <zhowes14@icloud.com>
2022-06-23 23:31:54 -0400

    added context provider to share login information and fuctions

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,41 @@
+import { useState, useEffect } from "react";
 import React from "react";
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
 });
+
+export const AuthContextProvider = (props) => {
+  useEffect(() => {
+    const storedUserLoggedInIfno = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLoggedInIfno === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+  const loginHandler = () => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
